69312dfe437298f011f051b9235feab0647902cf
Valentin Rieu <rieuvalentin3@gmail.com>
2023-05-07 16:22:26 +0200

    ajout variable globale

--- vernam.py
+++ vernam.py
@@ -20,6 +20,7 @@ letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H',
 def cipher(mess, key):
     global index
     global letters
+    global keys
     res = ""
 
     if len(mess)>len(key):
